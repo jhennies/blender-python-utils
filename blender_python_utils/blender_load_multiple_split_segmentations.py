ce63abe4df62ebf32493943888f3e94742d78ce0
jhennies <julianhennies@hotmail.de>
2025-02-04 11:43:33 +0100

    meshing now included

---
+++ blender_python_utils/blender_load_multiple_split_segmentations.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+from skimage.measure import marching_cubes
+
+import bpy
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+import os
+import json
+
+# with File('/media/julian/Data/projects/hennies/blender/inputs/hela_er_ds4_g1.2.h5', mode='r') as f:
+#     maskchannel = f['data'][:]
+
+source_folder = '/media/julian/Data/projects/khan/blender/data/labels_t69'
+pixel_spacing = np.array((0.6, 0.042, 0.042))
+json_fp = os.path.join(source_folder, 'pos.json')
+obj_fp = os.path.join(source_folder, 'obj_{:06d}.npy')
+
+name = 'golgi_{:05d}'
+
+# xy_scale = 0.01
+# z_scale = 0.01
+axes_order = 'zyx'
+thresh = 64
+
+init_scale = 0.02
+# scale = np.array([1, 1, z_scale / xy_scale]) * init_scale
+
+with open(json_fp, 'r') as f:
+    positions = json.load(f)
+
+for idx, pos in positions.items():
+
+    lbl = int(idx) + 1
+
+    maskchannel = np.load(obj_fp.format(lbl))
+
+    obj_array = np.moveaxis(maskchannel, [axes_order.find('x'), axes_order.find('y'), axes_order.find('z')], [0, 1, 2])
+
+    verts, faces, normals, values = marching_cubes(obj_array, thresh)
+
+    mesh = bpy.data.meshes.new(name.format(lbl))
+    mesh.from_pydata(verts, [], faces)
+    object_data_add(bpy.context, mesh)
+    obj = bpy.context.view_layer.objects.active
+    obj.location = pos
+    obj.scale = pixel_spacing
+    obj = bpy.context.active_object
+    attribute = obj.data.attributes.new(name="object id", type="INT", domain="POINT")
+    attribute.data.foreach_set('value', [0] * len(verts))
+
---
+++ blender_python_utils/blender_load_one_segmentation.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+from skimage.measure import marching_cubes
+
+import bpy
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from h5py import File
+
+with File('/media/julian/Data/projects/hennies/blender/inputs/hela_er_ds4_g1.2.h5', mode='r') as f:
+    maskchannel = f['data'][:]
+
+xy_scale = 0.01
+z_scale = 0.01
+axes_order = 'zyx'
+thresh = 64
+
+init_scale = 0.02
+scale = np.array([1, 1, z_scale / xy_scale]) * init_scale
+
+obj_array = np.moveaxis(maskchannel, [axes_order.find('x'), axes_order.find('y'), axes_order.find('z')], [0, 1, 2])
+
+verts, faces, normals, values = marching_cubes(obj_array, thresh)
+
+mesh = bpy.data.meshes.new('er_ds4_g1.2')
+mesh.from_pydata(verts, [], faces)
+object_data_add(bpy.context, mesh)
+obj = bpy.context.view_layer.objects.active
+obj.location = [0, 0, 0]
+obj.scale = scale
+obj = bpy.context.active_object
+attribute = obj.data.attributes.new(name="object id", type="INT", domain="POINT")
+attribute.data.foreach_set('value', [0] * len(verts))
+
